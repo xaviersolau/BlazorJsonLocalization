ef0b2a6b689ad1549b3392c7510a8a9b92487aec
Xavier Solau <xaviersolau@yahoo.fr>
2021-03-18 00:45:27 +0100

    Add unit tests: - Embedded json file loading service; - culture info service;

---
+++ src/tests/SoloX.BlazorJsonLocalization.UTests/Services/CultureInfoServiceTest.cs
@@ -0,0 +1,25 @@
+﻿// ----------------------------------------------------------------------
+// <copyright file="CultureInfoServiceTest.cs" company="Xavier Solau">
+// Copyright © 2021 Xavier Solau.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+// </copyright>
+// ----------------------------------------------------------------------
+
+using SoloX.BlazorJsonLocalization.Services.Impl;
+using System.Globalization;
+using Xunit;
+
+namespace SoloX.BlazorJsonLocalization.UTests.Services
+{
+    public class CultureInfoServiceTest
+    {
+        [Fact]
+        public void IsShouldGetCultureInfoFromStaticCurrentUICulture()
+        {
+            var service = new CultureInfoService();
+
+            Assert.Equal(CultureInfo.CurrentUICulture, service.CurrentUICulture);
+        }
+    }
+}
