0eda76b190de8010e50a149275310b980bc2dc2b
Xavier Solau <xaviersolau@yahoo.fr>
2023-02-08 00:24:28 +0100

    Add generator pattern tests.

--- src/tools/SoloX.BlazorJsonLocalization.Tools.Core/Patterns/LocalizerExtensions.cs
+++ src/tools/SoloX.BlazorJsonLocalization.Tools.Core/Patterns/LocalizerExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using Microsoft.Extensions.Localization;
 
@@ -9,7 +11,36 @@ namespace SoloX.BlazorJsonLocalization.Tools.Core.Patterns.Impl
     {
         public static IStringLocalizer GetSubLocalizer(this IStringLocalizer localizer, params string[] structuredKey)
         {
-            throw new NotImplementedException();
+            return new SubStringLocalizer(localizer, string.Join(":", structuredKey) + ":");
+        }
+    }
+
+    internal class SubStringLocalizer : IStringLocalizer
+    {
+        private readonly IStringLocalizer localizer;
+        private readonly string structuredKeyPrefix;
+
+        /// <summary>
+        /// Setup instance.
+        /// </summary>
+        /// <param name="localizer">Base localizer reference.</param>
+        /// <param name="structuredKeyPrefix">Structured key to use as prefix.</param>
+        public SubStringLocalizer(IStringLocalizer localizer, string structuredKeyPrefix)
+        {
+            this.localizer = localizer;
+            this.structuredKeyPrefix = structuredKeyPrefix;
+        }
+
+        ///<inheritdoc/>
+        public LocalizedString this[string name] => this.localizer[this.structuredKeyPrefix + name];
+
+        ///<inheritdoc/>
+        public LocalizedString this[string name, params object[] arguments] => this.localizer[this.structuredKeyPrefix + name, arguments];
+
+        ///<inheritdoc/>
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            return this.localizer.GetAllStrings(includeParentCultures).Where(s => s.Name.StartsWith(this.structuredKeyPrefix, StringComparison.InvariantCulture));
         }
     }
 }
