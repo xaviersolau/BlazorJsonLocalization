1163414a3e45e18937ec1253c09cafe2a5907304
Xavier Solau <xaviersolau@yahoo.fr>
2023-02-08 00:24:28 +0100

    Add generator pattern tests.

---
+++ src/tools.tests/SoloX.BlazorJsonLocalization.Tools.Core.UTests/PatternsTest.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Localization;
+using Moq;
+using SoloX.BlazorJsonLocalization.Tools.Core.Patterns.Impl;
+using SoloX.BlazorJsonLocalization.Tools.Core.Patterns.Itf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloX.BlazorJsonLocalization.Tools.Core.UTests
+{
+    public class PatternsTest
+    {
+        private readonly Mock<IStringLocalizer<MyObject>> mock;
+        private readonly IMyObjectStringLocalizerPattern pattern;
+
+        public PatternsTest()
+        {
+            this.mock = new Mock<IStringLocalizer<MyObject>>();
+            this.pattern = mock.Object.ToMyObjectStringLocalizerPattern();
+        }
+
+        [Fact]
+        public void IsShouldGetInterfaceImplementation()
+        {
+            pattern.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void IsShouldForwardMethodCallToStringLocalizer()
+        {
+            var expectedValue = "My Test Value";
+            var expectedName = "My Test Name";
+
+            var argument = "My Argument";
+
+            mock.Setup(x => x[nameof(IMyObjectStringLocalizerPattern.SomeStringArgs), argument]).Returns(new LocalizedString(expectedName, expectedValue));
+
+            var value = pattern.SomeStringArgs(argument);
+
+            value.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void IsShouldForwardPropertyGetToStringLocalizer()
+        {
+            var expectedValue = "My Test Value";
+            var expectedName = "My Test Name";
+
+            mock.Setup(x => x[nameof(IMyObjectStringLocalizerPattern.SomeProperty)]).Returns(new LocalizedString(expectedName, expectedValue));
+
+            var value = pattern.SomeProperty;
+
+            value.Should().Be(expectedValue);
+        }
+
+        [Fact]
+        public void IsShouldForwardSubPropertyGetToStringLocalizer()
+        {
+            var expectedValue = "My Test Value";
+            var expectedName = "My Test Name";
+
+            mock.Setup(x => x[nameof(IMyObjectStringLocalizerPattern.MyObjectSubStringLocalizerProperty) + ":" + nameof(IMyObjectSubStringLocalizerPattern.SomeSubProperty)]).Returns(new LocalizedString(expectedName, expectedValue));
+
+            var value = pattern.MyObjectSubStringLocalizerProperty.SomeSubProperty;
+
+            value.Should().Be(expectedValue);
+        }
+    }
+}
