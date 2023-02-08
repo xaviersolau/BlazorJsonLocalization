ed513f29a246cb87ba0309d6057b9c3bd54bf86d
Xavier Solau <xaviersolau@yahoo.fr>
2023-02-09 00:26:44 +0100

    Add NamingPolicyHandler option for embedded json resources. #48

---
+++ src/examples/SoloX.BlazorJsonLocalization.Example.Components.Embedded3/ComponentsEmbeddedExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloX.BlazorJsonLocalization.Example.Components.Embedded3
+{
+    public static class ComponentsEmbeddedExtensions
+    {
+        public static JsonLocalizationOptionsBuilder UseComponentsEmbedded3(this JsonLocalizationOptionsBuilder builder)
+        {
+            return builder.UseEmbeddedJson(options =>
+            {
+                options.Assemblies = new[] { typeof(ComponentsEmbeddedExtensions).Assembly };
+                options.NamingPolicy = (basePath, cultureName) => $"{basePath}.razor{(string.IsNullOrEmpty(cultureName) ? string.Empty : $"-{cultureName}")}.json";
+            });
+        }
+    }
+}
---
+++ src/examples/SoloX.BlazorJsonLocalization.Example.Components.Embedded3/Pages/Component2.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloX.BlazorJsonLocalization.Example.Components.Embedded3.Pages
+{
+    public partial class Component2
+    {
+        [Inject]
+        private IStringLocalizer<Component2> L { get; set; }
+    }
+}
